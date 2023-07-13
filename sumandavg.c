c5563f5435472ee09546aaffcfd696f133e8148b
saharathp <126144646+saharathp@users.noreply.github.com>
2023-07-13 21:05:29 +0545

    Add files via upload

---
+++ sumandavg.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void sumandavg(int a,int b,int *sum,float *avg)
+{
+  *sum=a+b;
+ *avg=(float)*sum/2;
+}
+int main(){
+    int i,j,sum;
+    float avg;
+    i=9;
+    j=8;
+    sumandavg(i,j,&sum,&avg);
+    printf("The value of sum is %d\n",sum);
+    printf("The value of avg is %f\n",avg);
+    return 0;
+}
